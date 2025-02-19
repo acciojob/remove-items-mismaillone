e0b0128967726c006660bcc329c70bc8cd4640c9
Mohammad Ismail <101501734+mismaillone@users.noreply.github.com>
2025-02-20 01:03:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+function removeSelect() {
+	const selectElement = document.getElementById('colorSelect');
+    const selectedOptions = selectElement.querySelectorAll('option:checked');
+
+    selectedOptions.forEach(option => option.remove());
+}
